eb62837189bccb1473e72fffff070a4c67684142
Ujjwal Sachdeva <ujjwal.sachdeva24@gmail.com>
2025-10-02 23:27:36 +0530

    bug fix

--- app/head.tsx
+++ app/head.tsx
@@ -3,7 +3,7 @@ export default function Head() {
     <>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="I'm Ujjwal Sachdeva, full-stack developer based in Pune, India. My journey in web development began during my first year of college." />
-      <meta name="keywords" content="Ujjwal Sachdeva, portfolio, jigar, full stack dev, nextjs portfolio, portfolio design, portfolio website, personal portfolio" />
+      <meta name="keywords" content="" />
       <meta property="og:site_name" content="Ujjwal Sachdeva's Portfolio" />
       <meta property="og:title" content="Portfolio | Ujjwal Sachdeva - Full Stack Developer" />
       <meta property="og:description"
